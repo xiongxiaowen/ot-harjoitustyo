bed7ac8e5519c0d806b0678d5596260619d95813
XiaowenOT <you@example.com>
2025-03-25 12:17:16 +0200

    Lisätty unicafe-projektin tiedostot

---
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -0,0 +1,119 @@
+﻿import unittest
+from maksukortti import Maksukortti
+# -*- coding: utf-8 -*-
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
+
+"""
+#This class for practice
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        print("Set up goes here")
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        # alustetaan maksukortti, jossa on 10 euroa (1000 senttiä)
+        kortti = Maksukortti(1000)
+        vastaus = str(kortti)
+
+        self.assertEqual(vastaus, "Kortilla on rahaa 10.00 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(str(kortti), "Kortilla on rahaa 7.50 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein_2(self): 
+        kortti = Maksukortti(1000)
+        kortti.syo_edullisesti()
+
+        # varmistetaan että saldoa jäljellä 7.5 euroa eli 750 senttiä
+        self.assertEqual(kortti.saldo_euroina(), 7.5)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 6.0)
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+"""
+
+class TestMaksukortti(unittest.TestCase):
+    def setUp(self):
+        self.kortti = Maksukortti(1000)
+    
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_edullisesti()
+
+        self.assertEqual(self.kortti.saldo_euroina(), 7.5)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        self.kortti.syo_maukkaasti()
+
+        self.assertEqual(self.kortti.saldo_euroina(), 6.0)    
+
+    def test_syo_edullisesti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.kortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 35.0)
+
+    def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
+        self.kortti.lataa_rahaa(20000)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+
+#below for task3
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        # Maukkaan lounaan syöminen ei vie saldoa negatiiviseksi
+        kortti = Maksukortti(350)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 3.5)
+
+    def test_negatiisen_ei_muuta_saldoa(self):
+        # Negatiivisen summan lataaminen ei muuta kortin saldoa
+        self.kortti.lataa_rahaa(-1000)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 10.0)
+
+#Kortilla pystyy ostamaan edullisen lounaan, kun kortilla rahaa vain edullisen lounaan verran (eli 2.5 euroa)
+    def test_kun_kortilla_vain_edullisenlounaan(self):
+        kortti = Maksukortti(250)
+
+        kortti.syo_edullisesti()
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+
+# Kortilla pystyy ostamaan maukkaan lounaan, kun kortilla rahaa vain maukkaan lounaan verran (eli 4 euroa)
+    def test_kun_kortilla_vain_maukkaanlounaan(self):
+        kortti = Maksukortti(400)
+        kortti.syo_maukkaasti()
+
+        self.assertEqual(kortti.saldo_euroina(), 0.0)
+
+
+
+
+
+
+
+
+        
